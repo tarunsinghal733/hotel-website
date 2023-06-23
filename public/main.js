bf367c99db93cf8ef25b74911095435fb61fdb30
tarunsinghal733 <tarunsinghal055@gmail.com>
2023-06-23 16:41:42 +0530

    e5us6r

---
+++ public/main.js
@@ -0,0 +1,11 @@
+//swiper
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: { el: ".swiper-pagination", 
+    clickable: true
+},
+autoplay: {
+    delay: 2500,
+},
+});
