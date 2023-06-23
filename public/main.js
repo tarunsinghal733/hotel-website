cbee41437610a034b03c788e3a9185a212d0424e
tarunsinghal733 <tarunsinghal055@gmail.com>
2023-06-23 21:51:54 +0530

    thrt

--- public/main.js
+++ public/main.js
@@ -29,3 +29,28 @@ autoplay: {
     delay: 3500,
 },
 });
+
+
+//*counter*//
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+
+                obj.textContent = current;
+
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 78, 1548, 3000);
+    counter("count2", 26, 2144, 3000);
+    counter("count3", 61, 2891, 3000);
+    counter("count4", 49, 3211, 3000);
+});
