91ad0883c0cf2507c1916667e0841c05266df1b4
tarunsinghal733 <tarunsinghal055@gmail.com>
2023-06-23 19:25:05 +0530

    vgbhnjtryj

--- public/main.js
+++ public/main.js
@@ -1,3 +1,23 @@
+//*Navbar*//
+let nav = document.querySelector(".navbar");
+window.onscroll = function() {
+  if (document.documentElement.scrollTop > 200) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+}
+
+//*Navbar -hide*//
+let navbar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navbar.forEach(function(a){
+    a.addEventListener("click", function(){
+navCollapse.classList.remove("show");
+    })
+})
+
+
 //swiper
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
@@ -6,6 +26,6 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true
 },
 autoplay: {
-    delay: 2500,
+    delay: 3500,
 },
 });
