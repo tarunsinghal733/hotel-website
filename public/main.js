884a185784cd458c87b98f0b6d0036bdee4d4590
tarunsinghal733 <tarunsinghal055@gmail.com>
2023-06-24 18:35:47 +0530

    tj

--- public/main.js
+++ public/main.js
@@ -1,19 +1,19 @@
-//*Navbar*//
+// Header Scroll
 let nav = document.querySelector(".navbar");
-window.onscroll = function() {
-  if (document.documentElement.scrollTop > 50) {
-    nav.classList.add("header-scrolled");
-  } else {
-    nav.classList.remove("header-scrolled");
-  }
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
 }
 
-//*Navbar-hide*//
-let navbar = document.querySelectorAll('.nav-link');
-let navCollapse = document.querySelector('.navbar-collapse.collapse');
-navbar.forEach(function(a){
-    a.addEventListener("click", function(){
-navCollapse.classList.remove("show");
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
     })
 })
 
